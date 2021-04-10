5c09d7b4d9b35ef04c091dc2d98e0e9f4bb1e73d
jarvis <1053123198@qq.com>
2021-04-10 23:46:02 +0800

    修复es扩展类bug，添加GroupByDate 根据时间类型聚合（未测试，待完善）

---
+++ ElasticSearch/CountAttribute.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SP.StudioCore.ElasticSearch
+{
+    /// <summary>
+    /// 文档综合
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Property)]
+    public class CountAttribute : Attribute
+    {
+
+    }
+}

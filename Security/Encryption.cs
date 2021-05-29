b921ddeea933f9f68829f2e9770e59b254d7e51f
peter <peter@bq8.com>
2021-05-30 02:01:05 +0800

    加入非Base64字符串的判断

--- Security/Encryption.cs
+++ Security/Encryption.cs
@@ -196,20 +196,27 @@ namespace SP.StudioCore.Security
         public static string AesDecrypt(string str, string key)
         {
             if (string.IsNullOrEmpty(str)) return null;
-            Byte[] toEncryptArray = Convert.FromBase64String(str);
-
-            using (RijndaelManaged rm = new RijndaelManaged
+            try
             {
-                Key = Encoding.UTF8.GetBytes(key),
-                Mode = CipherMode.ECB,
-                Padding = PaddingMode.PKCS7
-            })
+                Byte[] toEncryptArray = Convert.FromBase64String(str);
+
+                using (RijndaelManaged rm = new RijndaelManaged
+                {
+                    Key = Encoding.UTF8.GetBytes(key),
+                    Mode = CipherMode.ECB,
+                    Padding = PaddingMode.PKCS7
+                })
+                {
+
+                    ICryptoTransform cTransform = rm.CreateDecryptor();
+                    Byte[] resultArray = cTransform.TransformFinalBlock(toEncryptArray, 0, toEncryptArray.Length);
+
+                    return Encoding.UTF8.GetString(resultArray);
+                }
+            }
+            catch
             {
-
-                ICryptoTransform cTransform = rm.CreateDecryptor();
-                Byte[] resultArray = cTransform.TransformFinalBlock(toEncryptArray, 0, toEncryptArray.Length);
-
-                return Encoding.UTF8.GetString(resultArray);
+                return null;
             }
         }
     }

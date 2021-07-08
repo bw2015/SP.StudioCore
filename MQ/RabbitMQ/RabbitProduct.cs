08b765908e32f8b50b853c7627dd503cb8be645d
niao <niao@es3.com>
2021-07-08 15:51:31 +0800

    rabbit增加支持最大、最低空闲频道连接池的管理

--- MQ/RabbitMQ/RabbitProduct.cs
+++ MQ/RabbitMQ/RabbitProduct.cs
@@ -3,6 +3,7 @@ using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
 using RabbitMQ.Client;
@@ -13,7 +14,8 @@ namespace SP.StudioCore.MQ.RabbitMQ
 {
     public class RabbitProduct : IRabbitProduct
     {
-        private ConcurrentQueue<IModel> Stacks = new();
+        private readonly ConcurrentQueue<IModel> Stacks = new();
+
         /// <summary>
         /// 配置信息
         /// </summary>
@@ -25,27 +27,57 @@ namespace SP.StudioCore.MQ.RabbitMQ
         private readonly RabbitConnect _connect;
 
         private static readonly object objLock = new();
-        
+
         public RabbitProduct(RabbitConnect connect, ProductConfig productConfig)
         {
-            if (productConfig.MinChannelPool == 0) productConfig.MinChannelPool = 10;
+            if (productConfig.MinFreeChannelPool == 0) productConfig.MinFreeChannelPool = 3;
+            if (productConfig.MaxFreeChannelPool == 0) productConfig.MaxFreeChannelPool = 10;
             _connect       = connect;
             _productConfig = productConfig;
 
             _connect.Open();
+
             // 启动后，后台立即创建10个频道
-            Task.Factory.StartNew(() => 
+            KeepFeeChannelCount();
+        }
+
+        /// <summary>
+        /// 保持最低空闲频道数量
+        /// </summary>
+        private void KeepFeeChannelCount()
+        {
+            Task.Factory.StartNew(() =>
             {
-                for (int i = 0; i < productConfig.MinChannelPool; i++)
+                while (true)
                 {
-                    var channel = _connect.Connection.CreateModel();
-                    if (_productConfig.UseConfirmModel) channel.ConfirmSelect();
-                    Stacks.Enqueue(channel);
+                    // 当低于最低空闲的频道池时，快速创建到指定数量
+                    while (Stacks.Count < _productConfig.MinFreeChannelPool)
+                    {
+                        var channel = _connect.Connection.CreateModel();
+                        if (_productConfig.UseConfirmModel) channel.ConfirmSelect();
+                        Stacks.Enqueue(channel);
+                    }
+
+                    // 当超出最大空闲频道池时，则释放到指定的最大空闲频道
+                    while (Stacks.Count > _productConfig.MaxFreeChannelPool)
+                    {
+                        // 从池中取出频道
+                        var tryPop = Stacks.TryDequeue(out var channel);
+
+                        // 取出失败，说明没有可用频道，需要创建新的
+                        if (tryPop && channel is {IsClosed: false})
+                        {
+                            channel.Close();
+                            channel.Dispose();
+                        }
+                    }
+
+                    Thread.Sleep(TimeSpan.FromSeconds(3));
                 }
             });
         }
 
-        
+
         /// <summary>
         ///     开启生产消息
         /// </summary>
@@ -58,10 +90,10 @@ namespace SP.StudioCore.MQ.RabbitMQ
             {
                 // 从池中取出频道
                 var tryPop = Stacks.TryDequeue(out var channel);
-            
+
                 // 取出失败，说明没有可用频道，需要创建新的
                 if (tryPop && channel is {IsClosed: false}) return channel;
-                
+
                 channel = _connect.Connection.CreateModel();
                 if (_productConfig.UseConfirmModel) channel.ConfirmSelect();
                 return channel;
@@ -112,20 +144,20 @@ namespace SP.StudioCore.MQ.RabbitMQ
                 channel = CreateChannel();
                 if (sw2.ElapsedMilliseconds > 10) Console.WriteLine($"CreateChannel耗时：{sw2.ElapsedMilliseconds}");
                 sw2.Restart();
-                
+
                 var basicProperties = channel.CreateBasicProperties();
                 // 默认设置为消息持久化
                 if (funcBasicProperties != null) funcBasicProperties(basicProperties);
                 else basicProperties.DeliveryMode = 2;
                 if (sw2.ElapsedMilliseconds > 10) Console.WriteLine($"CreateBasicProperties耗时：{sw2.ElapsedMilliseconds}");
                 sw2.Restart();
-                
+
                 //消息内容
                 var body = Encoding.UTF8.GetBytes(message);
                 //发送消息
                 channel.BasicPublish(exchange: exchange, routingKey: routingKey, basicProperties: basicProperties, body: body);
-                var result =  !_productConfig.UseConfirmModel || channel.WaitForConfirms();
-                
+                var result = !_productConfig.UseConfirmModel || channel.WaitForConfirms();
+
                 if (sw2.ElapsedMilliseconds > 10) Console.WriteLine($"BasicPublish耗时：{sw2.ElapsedMilliseconds}");
                 return result;
             }

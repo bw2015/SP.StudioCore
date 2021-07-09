e7e1b7404d36188644b7462ed469201dc567d2ff
bw2015 <38880080+bw2015@users.noreply.github.com>
2021-07-09 17:07:47 +0800

    工具基類調整

--- Tools/ToolsProgram.cs
+++ Tools/ToolsProgram.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
 using System.Collections.Generic;
@@ -13,17 +14,33 @@ namespace SP.StudioCore.Tools
         protected static bool WebStartup(string[] args)
         {
             if (!args.Contains("--urls")) return false;
-
-            CreateHostBuilder(args).Build().Run();
-
+            CreateWebHostBuilder(args).Build().Run();
             return true;
         }
 
-        private static IHostBuilder CreateHostBuilder(string[] args) =>
+        protected static void WorkStartup(string[] args,
+            Action<HostBuilderContext, IServiceCollection> configureDelegate)
+        {
+            CreateWorkHostBuilder(args, configureDelegate).Build().Run(); 
+        }
+
+        protected static void WorkStartup<THostedService>(string[] args) where THostedService : class, IHostedService
+        {
+            CreateWorkHostBuilder(args, (hostContext, services) =>
+            {
+                services.AddHostedService<THostedService>();
+            }).Build().Run();
+        }
+
+        private static IHostBuilder CreateWebHostBuilder(string[] args) =>
            Host.CreateDefaultBuilder(args)
                .ConfigureWebHostDefaults(webBuilder =>
                {
                    webBuilder.UseStartup<ToolsStartup>();
                });
+
+        private static IHostBuilder CreateWorkHostBuilder(string[] args, Action<HostBuilderContext, IServiceCollection> configureDelegate) =>
+           Host.CreateDefaultBuilder(args)
+               .ConfigureServices(configureDelegate);
     }
 }

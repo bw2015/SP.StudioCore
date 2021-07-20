db2d911e8cb8725bc55beff48904dd7c3c295a7d
niao <niao@es3.com>
2021-07-20 22:48:29 +0800

    修改索引名称

--- LinkTrack/LinkTrackEs.cs
+++ LinkTrack/LinkTrackEs.cs
@@ -6,7 +6,7 @@ namespace SP.StudioCore.LinkTrack
 {
     public class LinkTrackEs : EsBase<LinkTrackEs, LinkTrackContextPO>
     {
-        public LinkTrackEs(DateTime? indexDateTime = null) : base("LinkTrack", new[] {"LinkTrack"}, 0, 3, indexDateTime)
+        public LinkTrackEs(DateTime? indexDateTime = null) : base("link_track", new[] {"link_track"}, 0, 3, indexDateTime)
         {
         }
     }

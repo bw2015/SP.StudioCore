7851eac62babcfb6cb69cea98d836824db54d492
Alienware <betwin2015@mail.com>
2021-09-28 20:19:15 +0800

    消息队列的任务进度接口

---
+++ MQ/IMessageTask.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SP.StudioCore.MQ
+{
+    /// <summary>
+    /// 标记一个任务ID
+    /// </summary>
+    public interface IMessageTask
+    {
+        /// <summary>
+        /// 任务ID
+        /// </summary>
+        Guid TaskID { get; set; }
+    }
+}
